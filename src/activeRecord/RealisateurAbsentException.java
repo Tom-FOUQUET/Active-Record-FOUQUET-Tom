c5c398fce46ce6511bb49205353bcd3d56f8226e
unknown <fouquettom2004@gmail.com>
2023-11-28 15:12:11 +0100

    ajout de l'exception

--- src/activeRecord/RealisateurAbsentException.java
+++ src/activeRecord/RealisateurAbsentException.java
@@ -1,4 +1,9 @@
 package activeRecord;
 
 public class RealisateurAbsentException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "Le realisateur n'existe pas";
+    }
 }
